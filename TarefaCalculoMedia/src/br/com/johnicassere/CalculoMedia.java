9d4d00d8ac489d1e75fbb9ed6353b6bf6ca27f80
johnicassere <johnicassere@hotmail.com>
2024-10-14 11:11:49 -0300

    calculo media

---
+++ TarefaCalculoMedia/src/br/com/johnicassere/CalculoMedia.java
@@ -0,0 +1,16 @@
+package br.com.johnicassere;
+
+public class CalculoMedia {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 15;
+		int num3 = 25;
+		int num4 = 35;
+		int resultado = (num1 + num2 + num3 + num4) /4;
+		
+		System.out.println(resultado);
+
+	}
+
+}
